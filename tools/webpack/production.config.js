9dcf42882ea6e62a98893ddd7957671634329f85
Rocco <roccorighi@hotmail.com>
2018-05-09 20:40:04 +0100

    Add css loader to webpack.production

--- tools/webpack/production.config.js
+++ tools/webpack/production.config.js
@@ -5,19 +5,23 @@ const DIST_DIR = path.join(__dirname, '..', '..', 'dist');
 const SRC_DIR = path.join(__dirname, '..', '..', 'src');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-      }
-    ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR,
-  },
-  plugins: [ new HtmlWebpackPlugin() ],
+    entry: `${SRC_DIR}/index.js`,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: SRC_DIR,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: DIST_DIR,
+    },
+    plugins: [ new HtmlWebpackPlugin() ],
 };
